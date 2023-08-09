8406be64b206eb567cd5e33653bc015b01d76d54
bervalim <bernardogvalim@gmail.com>
2023-08-09 20:05:50 -0300

    kenzieHub

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -3,7 +3,7 @@ import { LoginForm } from "../../components/LoginForm";
 export const Login = () => {
   return (
     <main>
-      <h1>Login</h1>
+      {/* <h1>Login</h1> */}
       <LoginForm />
     </main>
   );
--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -3,7 +3,8 @@ import { RegisterForm } from "../../components/RegisterForm";
 export const Register = () => {
   return (
     <main>
-      <h1>Register</h1>
+      {/* <h3>Crie sua conta</h3>
+      <p>Rápido e grátis,vamos nessa</p> */}
       <RegisterForm />
     </main>
   );
