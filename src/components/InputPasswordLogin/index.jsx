8785c1cec9c2431e4be37e1125f391473face155
bervalim <bernardogvalim@gmail.com>
2023-08-09 21:07:39 -0300

    kenzieHub

--- src/components/InputPasswordLogin/index.jsx
+++ src/components/InputPasswordLogin/index.jsx
@@ -5,13 +5,13 @@ export const InputPassword = forwardRef(({ error, label, ...rest }, ref) => {
   const [isHidden, setIsHidden] = useState(true);
 
   return (
-    <>
+    <div>
       <label className="label">{label}</label>
       <input type={isHidden ? "password" : "text"} ref={ref} {...rest} />
       <button type="button" onClick={() => setIsHidden(!isHidden)}>
         {isHidden ? <MdVisibility /> : <MdVisibilityOff />}
       </button>
       {error ? <p>{error.message}</p> : null}
-    </>
+    </div>
   );
 });
