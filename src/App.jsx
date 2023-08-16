727a59c80648371d87a4c6da32c2dd716b23192f
bervalim <bernardogvalim@gmail.com>
2023-08-16 17:25:14 -0300

    kenzieHub

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import "./styles/index.scss";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { UserContext } from "./providers/UserContext";
+import { Loading } from "./components/Loading";
 
 export const App = () => {
   const { loading } = useContext(UserContext);
   return (
     <div className="App">
-      {loading ? <p>Carregando...</p> : <RoutesMain />}
+      {loading ? <Loading /> : <RoutesMain />}
       <ToastContainer position="top-right" autoClose={2000} />
     </div>
   );
