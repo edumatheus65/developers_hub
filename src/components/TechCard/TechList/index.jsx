60856e01c90e2db5247951c9b3a12887c0cb03cf
bervalim <bernardogvalim@gmail.com>
2023-08-24 17:58:22 -0300

    kenzieHub

--- src/components/TechCard/TechList/index.jsx
+++ src/components/TechCard/TechList/index.jsx
@@ -2,31 +2,36 @@ import { useContext } from "react";
 import { BiSolidPencil } from "react-icons/bi";
 import { BsTrash } from "react-icons/bs";
 import { TechContext } from "../../../providers/TechContext";
+import styles from "./style.module.scss";
 
 export const TechList = ({ tech }) => {
   const { deleteTech, setEditingTech } = useContext(TechContext);
 
   return (
-    <li>
-      <h3 className="techName">{tech.title}</h3>
-      <div>
+    <li className={styles.techItem}>
+      <div className={styles.techName}>
+        <h3 className="techName">{tech.title}</h3>
+      </div>
+      <div className={styles.techIcons}>
         <p className="paragraph">{tech.status}</p>
-        <button
-          className="iconsColor"
-          onClick={() => setEditingTech(tech)}
-          title="Editar tecnologia"
-          aria-label="edit"
-        >
-          <BiSolidPencil size={15} />
-        </button>
-        <button
-          className="iconsColor"
-          onClick={() => deleteTech(tech.id)}
-          title="Deletar tecnologia"
-          aria-label="remove"
-        >
-          <BsTrash size={16} />
-        </button>
+        <div className={styles.icons}>
+          <button
+            className="iconsColor"
+            onClick={() => setEditingTech(tech)}
+            title="Editar tecnologia"
+            aria-label="edit"
+          >
+            <BiSolidPencil size={15} />
+          </button>
+          <button
+            className="iconsColor"
+            onClick={() => deleteTech(tech.id)}
+            title="Deletar tecnologia"
+            aria-label="remove"
+          >
+            <BsTrash size={16} />
+          </button>
+        </div>
       </div>
     </li>
   );
--- src/components/TechCard/index.jsx
+++ src/components/TechCard/index.jsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { TechContext } from "../../providers/TechContext";
 import { TechList } from "./TechList";
+import styles from "./style.module.scss";
 
 export const TechCard = () => {
   const { techList } = useContext(TechContext);
   return (
-    <ul>
+    <ul className={styles.techList}>
       {techList.map((tech) => (
         <TechList key={tech.id} tech={tech} />
       ))}
